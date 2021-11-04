41da531ed91403cf94218bd691674d60bb23fb6a
Sourav Pahwa <70853906+Sourav61@users.noreply.github.com>
2021-11-04 13:13:05 +0530

    Update and rename Print alternate elements of an array to Print alternate elements of an array.cpp

--- Basic/Print alternate elements of an array.cpp
+++ Basic/Print alternate elements of an array.cpp
@@ -1,4 +1,4 @@
-https://practice.geeksforgeeks.org/problems/print-alternate-elements-of-an-array/1/?category[]=Arrays&category[]=Arrays&problemStatus=unsolved&difficulty[]=-2&page=1&query=category[]ArraysproblemStatusunsolveddifficulty[]-2page1category[]Arrays
+// https://practice.geeksforgeeks.org/problems/print-alternate-elements-of-an-array/1/?category[]=Arrays&category[]=Arrays&problemStatus=unsolved&difficulty[]=-2&page=1&query=category[]ArraysproblemStatusunsolveddifficulty[]-2page1category[]Arrays
 
 // { Driver Code Starts
 //Initial Template for C++
