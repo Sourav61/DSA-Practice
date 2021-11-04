6625c188fe92897b0008d5419c6656f7509b725d
Sourav Pahwa <70853906+Sourav61@users.noreply.github.com>
2021-11-04 13:03:04 +0530

    Update and rename Learn to Comment to LearnToComment.cpp

--- Easy/LearnToComment.cpp
+++ Easy/LearnToComment.cpp
@@ -1,4 +1,4 @@
-https://practice.geeksforgeeks.org/problems/learn-to-comment/1/?problemStatus=solved&difficulty[]=0&page=1&query=problemStatussolveddifficulty[]0page1
+// https://practice.geeksforgeeks.org/problems/learn-to-comment/1/?problemStatus=solved&difficulty[]=0&page=1&query=problemStatussolveddifficulty[]0page1
 
 // { Driver Code Starts
 //Initial Template for C
