eee50ee3de7641fb79145bd0deee81165ba884d3
Sourav Pahwa <70853906+Sourav61@users.noreply.github.com>
2021-11-04 13:21:29 +0530

    Update and rename Find n-th term to Find n-th term.cpp

--- School/Find n-th term.cpp
+++ School/Find n-th term.cpp
@@ -1,4 +1,4 @@
-https://practice.geeksforgeeks.org/problems/find-n-th-term-of-series-1-3-6-10-15-215506/1/?problemStatus=solved&difficulty[]=-2&page=1&query=problemStatussolveddifficulty[]-2page1
+// https://practice.geeksforgeeks.org/problems/find-n-th-term-of-series-1-3-6-10-15-215506/1/?problemStatus=solved&difficulty[]=-2&page=1&query=problemStatussolveddifficulty[]-2page1
 
 // { Driver Code Starts
 #include <bits/stdc++.h>
