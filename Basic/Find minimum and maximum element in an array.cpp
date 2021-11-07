1e39ec068cbb00b3827f0a49d31627e90359dbe7
Sourav Pahwa <70853906+Sourav61@users.noreply.github.com>
2021-11-07 20:09:43 +0530

    Update and rename Find minimum and maximum element in an array to Find minimum and maximum element in an array.cpp

--- Basic/Find minimum and maximum element in an array.cpp
+++ Basic/Find minimum and maximum element in an array.cpp
@@ -1,4 +1,4 @@
-https://practice.geeksforgeeks.org/problems/find-minimum-and-maximum-element-in-an-array4428/1/?category[]=Arrays&category[]=Arrays&problemStatus=unsolved&difficulty[]=-2&difficulty[]=-1&page=1&query=category[]ArraysproblemStatusunsolveddifficulty[]-2difficulty[]-1page1category[]Arrays#
+// https://practice.geeksforgeeks.org/problems/find-minimum-and-maximum-element-in-an-array4428/1/?category[]=Arrays&category[]=Arrays&problemStatus=unsolved&difficulty[]=-2&difficulty[]=-1&page=1&query=category[]ArraysproblemStatusunsolveddifficulty[]-2difficulty[]-1page1category[]Arrays#
 
 // { Driver Code Starts
 #include <bits/stdc++.h>
