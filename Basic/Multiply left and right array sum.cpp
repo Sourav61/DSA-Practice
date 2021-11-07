3dd11776836cb5e24ccd807c109b2321b7f5c4c6
Sourav Pahwa <70853906+Sourav61@users.noreply.github.com>
2021-11-07 20:19:30 +0530

    Update and rename Multiply left and right array sum. to Multiply left and right array sum.cpp

--- Basic/Multiply left and right array sum.cpp
+++ Basic/Multiply left and right array sum.cpp
@@ -1,4 +1,4 @@
-https://practice.geeksforgeeks.org/problems/multiply-left-and-right-array-sum1555/1/?category[]=Arrays&category[]=Arrays&problemStatus=unsolved&difficulty[]=-2&difficulty[]=-1&page=1&query=category[]ArraysproblemStatusunsolveddifficulty[]-2difficulty[]-1page1category[]Arrays#
+// https://practice.geeksforgeeks.org/problems/multiply-left-and-right-array-sum1555/1/?category[]=Arrays&category[]=Arrays&problemStatus=unsolved&difficulty[]=-2&difficulty[]=-1&page=1&query=category[]ArraysproblemStatusunsolveddifficulty[]-2difficulty[]-1page1category[]Arrays#
 
 // { Driver Code Starts
 //Initial Template for C++
