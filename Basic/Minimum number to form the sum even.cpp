dc1c1be93fbc776b7c6d39449ebec55c0792c5c6
Sourav Pahwa <70853906+Sourav61@users.noreply.github.com>
2021-11-07 22:33:49 +0530

    Update and rename Minimum number to form the sum even to Minimum number to form the sum even.cpp

--- Basic/Minimum number to form the sum even.cpp
+++ Basic/Minimum number to form the sum even.cpp
@@ -1,4 +1,4 @@
-https://practice.geeksforgeeks.org/problems/minimum-number-to-form-the-sum-even0326/1/?category[]=Arrays&category[]=Arrays&problemStatus=unsolved&difficulty[]=-2&difficulty[]=-1&page=1&query=category[]ArraysproblemStatusunsolveddifficulty[]-2difficulty[]-1page1category[]Arrays
+// https://practice.geeksforgeeks.org/problems/minimum-number-to-form-the-sum-even0326/1/?category[]=Arrays&category[]=Arrays&problemStatus=unsolved&difficulty[]=-2&difficulty[]=-1&page=1&query=category[]ArraysproblemStatusunsolveddifficulty[]-2difficulty[]-1page1category[]Arrays
 
 // { Driver Code Starts
 #include <bits/stdc++.h>
