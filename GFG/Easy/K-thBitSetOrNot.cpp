eafff387e6c4ef23254de6d496fbbede076b4e8e
sourav pahwa <Sourav61pahwa@gmail.com>
2022-04-23 01:43:39 +0530

    Check whether K-th bit is set or not
    K-thBitSetOrNot Easy Difficulty GFG

---
+++ GFG/Easy/K-thBitSetOrNot.cpp
@@ -0,0 +1,42 @@
+// https://practice.geeksforgeeks.org/problems/check-whether-k-th-bit-is-set-or-not-1587115620/1/?page=1&difficulty[]=-1&difficulty[]=0&status[]=unsolved&category[]=Bit%20Magic&sortBy=submissions#
+
+// { Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution
+{
+public:
+    // Function to check if Kth bit is set or not.
+    bool checkKthBit(int n, int k)
+    {
+        return (n & (1 << k));
+    }
+};
+
+// { Driver Code Starts.
+
+// Driver Code
+int main()
+{
+    int t;
+    cin >> t; // taking testcases
+    while (t--)
+    {
+        long long n;
+        cin >> n; // input n
+        int k;
+        cin >> k; // bit number k
+        Solution obj;
+        if (obj.checkKthBit(n, k))
+            cout << "Yes" << endl;
+        else
+            cout << "No" << endl;
+    }
+    return 0;
+} // } Driver Code Ends
