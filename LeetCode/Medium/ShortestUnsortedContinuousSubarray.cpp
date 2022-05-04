525a56ba68e0a1c57be7d8dd6c9dac5d0eda3b95
sourav pahwa <Sourav61pahwa@gmail.com>
2022-05-04 19:24:01 +0530

    1679. Max Number of K-Sum Pairs
    1679. Max Number of K-Sum Pairs
    
    Leetcode Medium Difficulty

--- LeetCode/Medium/ShortestUnsortedContinuousSubarray.cpp
+++ LeetCode/Medium/ShortestUnsortedContinuousSubarray.cpp
@@ -8,6 +8,31 @@ class Solution
 public:
     int findUnsortedSubarray(vector<int> &nums)
     {
+        // Using Two Pointer Approach (1st Method)
+
+        int n = nums.size();
+        int leastSoFar = INT_MAX, left = 0, right = 0;
+        for (int i = n - 1; i >= 0; i--)
+        {
+            leastSoFar = min(leastSoFar, nums[i]);
+            if (leastSoFar != nums[i])
+            {
+                left = i;
+            }
+        }
+        int maxSoFar = INT_MIN;
+        for (int i = 0; i < n; i++)
+        {
+            maxSoFar = max(maxSoFar, nums[i]);
+            if (maxSoFar != nums[i])
+            {
+                right = i + 1;
+            }
+        }
+        return right - left;
+
+        // (2nd Method)
+
         int start = -1, end = -1, max = INT_MIN;
         for (int i = 0; i < nums.size(); i++)
         {
