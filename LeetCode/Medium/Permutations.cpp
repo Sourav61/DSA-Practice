d8ff4ac3b3ce75794ce692a5665d3c8b7b11ff67
sourav pahwa <Sourav61pahwa@gmail.com>
2022-05-18 01:51:07 +0530

    Covered Till Video Lecture 13
    Covered Till Video Lecture 13
    Approach-2

--- LeetCode/Medium/Permutations.cpp
+++ LeetCode/Medium/Permutations.cpp
@@ -5,30 +5,58 @@ using namespace std;
 
 class Solution
 {
-
     // Approach-1 (Uses extra space by using an array for counting freq)
     // Time Complexity => O(N!)*O(N)
     // Space Complexity => O(N)*O(N)
     // Auxilliary Space => O(N)
+
+    // private:
+    //      void findPermute(vector<int> &ds,vector<int> &n,vector<vector<int>> &ans,int arr[]){
+    //         if(ds.size() == n.size()){
+    //             ans.push_back(ds);
+    //             return;
+    //         }
+
+    //         for(int i=0;i<n.size();i++){
+    //             if(!arr[i]){
+    //                 ds.push_back(n[i]);
+    //                 arr[i]=1;
+    //                 findPermute(ds,n,ans,arr);
+    //                 arr[i]=0;
+    //                 ds.pop_back();
+    //             }
+    //         }
+    //     }
+
+    // public:
+    //     vector<vector<int>> permute(vector<int>& nums) {
+    //         vector<vector<int>> ans;
+    //         vector<int> ds;
+    //         int freq[nums.size()];
+
+    //         for(int i=0;i<nums.size();i++){
+    //             freq[i]=0;
+    //         }
+
+    //         findPermute(ds,nums,ans,freq);
+
+    //         return ans;
+    //     }
+
 private:
-    void findPermute(vector<int> &ds, vector<int> &n, vector<vector<int>> &ans, int arr[])
+    void findPermute(int ind, vector<int> &nums, vector<vector<int>> &ans)
     {
-        if (ds.size() == n.size())
+        if (ind == nums.size())
         {
-            ans.push_back(ds);
+            ans.push_back(nums);
             return;
         }
 
-        for (int i = 0; i < n.size(); i++)
+        for (int i = ind; i < nums.size(); i++)
         {
-            if (!arr[i])
-            {
-                ds.push_back(n[i]);
-                arr[i] = 1;
-                findPermute(ds, n, ans, arr);
-                arr[i] = 0;
-                ds.pop_back();
-            }
+            swap(nums[ind], nums[i]);
+            findPermute(ind + 1, nums, ans);
+            swap(nums[ind], nums[i]);
         }
     }
 
@@ -36,15 +64,8 @@ public:
     vector<vector<int>> permute(vector<int> &nums)
     {
         vector<vector<int>> ans;
-        vector<int> ds;
-        int freq[nums.size()];
 
-        for (int i = 0; i < nums.size(); i++)
-        {
-            freq[i] = 0;
-        }
-
-        findPermute(ds, nums, ans, freq);
+        findPermute(0, nums, ans);
 
         return ans;
     }
