d49dcd3187b5606dae8e713aca6f3e4f7223cb5f
sourav pahwa <Sourav61pahwa@gmail.com>
2022-05-23 13:39:53 +0530

    Odd Even Linked List
    328. Odd Even Linked List
    Medium Difficulty Leetcode

--- GFG/Medium/RearrangeLinkedList.cpp
+++ GFG/Medium/RearrangeLinkedList.cpp
@@ -4,31 +4,30 @@
 // C program to find n'th Node in linked list
 #include <stdio.h>
 #include <stdlib.h>
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 /* Link list Node */
 struct Node
 {
     int data;
-    struct Node* next;
-    
-    Node(int x){
+    struct Node *next;
+
+    Node(int x)
+    {
         data = x;
         next = NULL;
     }
 };
 
-
-
- // } Driver Code Ends
+// } Driver Code Ends
 /* Node structure
 
 struct Node
 {
     int data;
     struct Node* next;
-    
+
     Node(int x){
         data = x;
         next = NULL;
@@ -38,58 +37,55 @@ struct Node
 
 class Solution
 {
-    public:
+public:
     void rearrangeEvenOdd(Node *head)
-   {
-      Node* odd = head;
-      Node* even = head->next;
-      Node* temp = even;
-      while(even && even->next)
-      {
-        
-        odd->next = even->next;
-        odd = odd->next;
-        even->next = odd->next;
-        even = even->next;
-        
-      }
-      odd->next = temp;
-      
-   }
-};
+    {
+        Node *odd = head;
+        Node *even = head->next;
+        Node *temp = even;
+        while (even && even->next)
+        {
 
+            odd->next = even->next;
+            odd = odd->next;
+            even->next = odd->next;
+            even = even->next;
+        }
+        odd->next = temp;
+    }
+};
 
 // { Driver Code Starts.
 
 /* Driver program to test above function*/
 int main()
 {
-    int T,i,n,l;
+    int T, i, n, l;
 
-    cin>>T;
+    cin >> T;
 
-    while(T--)
+    while (T--)
     {
         struct Node *head = NULL;
         struct Node *temp = head;
-        cin>>n;
-        for(i=1; i<=n; i++)
+        cin >> n;
+        for (i = 1; i <= n; i++)
         {
-            cin>>l;
+            cin >> l;
 
             if (head == NULL)
-            {   
-                head = temp = new Node(l); 
-                
+            {
+                head = temp = new Node(l);
             }
             else
-            {  temp->next = new Node(l);
-               temp = temp->next;
+            {
+                temp->next = new Node(l);
+                temp = temp->next;
             }
         }
         Solution ob;
         ob.rearrangeEvenOdd(head);
-        while(head != NULL)
+        while (head != NULL)
         {
             printf("%d ", head->data);
             head = head->next;
@@ -98,4 +94,4 @@ int main()
     }
     return 0;
 }
-  // } Driver Code Ends
+// } Driver Code Ends
