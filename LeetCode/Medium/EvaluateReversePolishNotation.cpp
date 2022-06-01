14e5c917902edf99a10db9ebdde2527669bc170d
sourav pahwa <Sourav61pahwa@gmail.com>
2022-06-01 13:21:34 +0530

    150. Evaluate Reverse Polish Notation, Infix to Postfix, Evaluation of Postfix Expression
    150. Evaluate Reverse Polish Notation
    Medium Difficulty Leetcode
    
    Infix to Postfix
    Easy Difficulty GFG
    
    Evaluation of Postfix Expression
    Easy Difficulty GFG

---
+++ LeetCode/Medium/EvaluateReversePolishNotation.cpp
@@ -0,0 +1,41 @@
+// https://leetcode.com/problems/evaluate-reverse-polish-notation/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        stack<int> st;
+        for(auto s:tokens) {
+            if(s.size()>1 || isdigit(s[0])){
+                st.push(stoi(s));
+            }else{
+                auto b = st.top();
+                st.pop();
+
+                auto a = st.top();
+                st.pop();
+
+                switch(s[0]){
+                    case '+':
+                        st.push(a+b);
+                        break;
+                    case '-':
+                        st.push(a-b);
+                        break;
+                    case '*':
+                        st.push(a*b);
+                        break;
+                    case '/':
+                        st.push(a/b);
+                        break;
+                    case '^':
+                        st.push(pow(a,b));
+                        break;
+                }
+            }
+        }
+        return st.top();
+    }
+};
