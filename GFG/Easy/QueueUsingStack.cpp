851aecc0967f02cf6895be275d39b47cad50f2e1
sourav pahwa <Sourav61pahwa@gmail.com>
2022-06-02 14:13:38 +0530

    Queue using stack, 232. Implement Queue using Stacks
    Queue using stack
    Easy Difficulty GFG
    
    232. Implement Queue using Stacks
    Easy Difficulty Leetcode

---
+++ GFG/Easy/QueueUsingStack.cpp
@@ -0,0 +1,66 @@
+// https://practice.geeksforgeeks.org/problems/queue-using-stack/1/#
+
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Queue {
+    stack<int> s1, s2;
+public:
+
+    void enqueue(int x) {
+        s1.push(x);
+    }
+
+    int dequeue() {
+        if(s1.empty() and s2.empty()){
+            return -1;
+        }
+
+        if(s2.empty()){
+            while(!s1.empty()){
+                s2.push(s1.top());
+                s1.pop();
+            }
+        }
+
+        int topVal = s2.top();
+        s2.pop();
+
+        return topVal;
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        Queue ob;
+
+        int Q;
+        cin>>Q;
+        while(Q--){
+        int QueryType=0;
+        cin>>QueryType;
+        if(QueryType==1)
+        {
+            int a;
+            cin>>a;
+            ob.enqueue(a);
+        }
+        else if(QueryType==2)
+        {
+            cout<<ob.dequeue()<<" ";
+
+        }
+        }
+    cout<<endl;
+    }
+}
+  // } Driver Code Endss
